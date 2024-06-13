82651221c2fddabae5572d56c6cc89bd354ad191
EnriShtjefni <enrishtjefni2000@gmail.com>
2024-06-13 15:59:08 +0200

    Added some styling for the part of dropdown list the category filtering.

--- public/js/multipleSelectCategory.js
+++ public/js/multipleSelectCategory.js
@@ -1,7 +1,29 @@
-$(document).ready(function () {
-    $('#category-element').select2({
+document.addEventListener("DOMContentLoaded", function() {
+    const categoryElement = $('#category-element');
+
+    categoryElement.select2({
         placeholder: "-",
         allowClear: true,
-        closeOnSelect: false
+        closeOnSelect: false,
+        templateResult: formatState,
+        dropdownCssClass: 'select2-dropdown-custom'
     });
+
+    categoryElement.on('select2:select', function (e) {
+        updateOptionIcon(e.params.data.id, true);
+    });
+
+    categoryElement.on('select2:unselect', function (e) {
+        updateOptionIcon(e.params.data.id, false);
+    });
+
+    function formatState(state) {
+        return $(
+            '<span>' + state.text + '<span class="select2-icon" data-id="' + state.id + '">' + (state.selected ? '-' : '+') + '</span></span>'
+        );
+    }
+
+    function updateOptionIcon(id, isSelected) {
+        $('.select2-dropdown-custom .select2-results__option[id$="' + id + '"] .select2-icon').text(isSelected ? '-' : '+');
+    }
 });
